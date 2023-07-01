e933b261de59de0a64f6b4eea30744b9187bafde
Ioannis Mentesidis <51725514+GiannisJOHN@users.noreply.github.com>
2023-07-02 00:29:17 +0200

    responsive footer done

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,4 +1,11 @@
 import logo from '../../assets/shared/desktop/logo-light.png'
+import facebook from '../../assets/shared/desktop/icon-facebook.svg'
+import youtube from '../../assets/shared/desktop/icon-youtube.svg'
+import twitter from '../../assets/shared/desktop/icon-twitter.svg'
+import pinterest from '../../assets/shared/desktop/icon-pinterest.svg'
+import instagram from '../../assets/shared/desktop/icon-instagram.svg'
+
+
 
 import React from 'react'
 
@@ -8,13 +15,14 @@ function Footer() {
             <div className="container">
                 <div className="footer-navbar">
                     <img className='footer-logo' src={logo} alt="" />
+                    <hr className='footer-line-style footer-line-under-img'/>
                     <ul className='footer-navbar-list'>
                         <li>OUR COMPANY</li>
                         <li>LOCATIONS</li>
                         <li>CONTACT</li>
                     </ul>
                 </div>
-                <hr className='footer-line'/>
+                <hr className='footer-line-style footer-line'/>
                 <div className="footer-contact">
                     <div className='footer-contact-info'>
                         <h1 className='footer-text footer-text-bold'>Designo Central Office</h1>
@@ -25,6 +33,13 @@ function Footer() {
                         <h1 className='footer-text footer-text-bold'>Contact Us (Central Office)</h1>
                         <p className='footer-text footer-text-bold'>P : +1 253-863-8967</p>
                         <p className='footer-text footer-text-bold'>M : contact@designo.co</p>
+                    </div>
+                    <div className='footer-social-media'>
+                        <img src={facebook} alt="" />
+                        <img src={youtube} alt="" />
+                        <img src={twitter} alt="" />
+                        <img src={pinterest} alt="" />
+                        <img src={instagram} alt="" />
                     </div>
                 </div>
             </div>
