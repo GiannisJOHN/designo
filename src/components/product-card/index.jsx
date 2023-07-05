bf05b0250667b692365fe62480809e3925511854
Ioannis Mentesidis <51725514+GiannisJOHN@users.noreply.github.com>
2023-07-05 06:43:46 +0200

    grid images improved

--- src/components/product-card/index.jsx
+++ src/components/product-card/index.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function ProductCard(props) {
     return (
         <div className='product-card'>
-            <img className='product-card-img' src={props.data.img} alt="" />
+            <div className="product-card-img-container">
+                <img className='product-card-img' src={props.data.img} alt="" />
+            </div>
             <div className="product-card-info-container">
-                <h1>{props.data.title}</h1>
-                <p>{props.data.text}</p>
+                <h1 className='product-card-title'>{props.data.title}</h1>
+                <p className='paragraph product-card-text'>{props.data.text}</p>
             </div>
         </div>
     )
